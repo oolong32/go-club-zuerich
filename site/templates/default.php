d374bbf05b5ff291882a14ec95003496663a9008
oolong32 <josef.renner@gmail.com>
2025-05-11 23:01:40 +0200

    add image to news-item

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,10 +1,14 @@
-
 <?php snippet('header') ?>
 
   <main>
-    <h2><?= $page->title() ?></h2>
+    <?= $page->text()->kirbytext() ?>
 
-   <div> <?= $page->text() ?> </div>
+    <!-- das muss optional sein, und ein feld dafür haben -->
+    <figure>
+      <img src="/assets/img/parki.jpg" alt="A photo of our club location.">
+      <!-- this should come from a field -->
+    </figure>
+
 
     <aside class="template-marker">Template: Default</aside>
   </main>
