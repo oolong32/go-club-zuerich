fef206f8cd0a29040a9854e3aa745d8887725124
oolong32 <josef.renner@gmail.com>
2025-05-15 14:13:29 +0200

    breakpoint, language-switch

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -19,22 +19,6 @@
       </a>
     </h1>
 
-    <nav>
-      <ul class="page-navigation">
-        <?php foreach ($site->children()->listed() as $item): ?>
-        <li><a class="unstyled-link" href="<?= $item->url() ?>"><?= $item->naviName() ?></a></li>
-        <?php endforeach ?>
-      </ul>
-
-      <ul class="language-switch">
-        <?php foreach($kirby->languages() as $language): ?>
-        <li<?php e($kirby->language() == $language, ' class="active"') ?>>
-          <a href="<?= $page->url($language->code()) ?>" hreflang="<?php echo $language->code() ?>">
-            <?= html($language->name()) ?>
-          </a>
-        </li>
-        <?php endforeach ?>
-      </ul>
-    </nav>
+    <?php snippet('nav') ?>
 
   </header>
