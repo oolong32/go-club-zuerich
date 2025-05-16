74855e1651ddb14c2edf59d587a4be24f8d226b8
oolong32 <josef.renner@gmail.com>
2025-05-16 22:11:36 +0200

    restore accidentally deleted fields

--- site/snippets/tournament-registration.php
+++ site/snippets/tournament-registration.php
@@ -7,6 +7,12 @@
   <input type="text" name="name" id="name" value="<?= $data['name'] ?? null ?>" required>
 </section>
 
+
+<section class="form-element">
+  <label for="email">E-Mail <abbr title="required">*</abbr></label>
+  <input type="email" name="email" id="email" value="<?= $data['email'] ?? null ?>" required>
+</section>
+
 <section class="form-element">
   <label for="rank"><?php echo t('strength') ?></label>
   <select id="rank" name="rank" value="<?= $data['rank'] ?? null ?>">
@@ -50,6 +56,52 @@
     <option>29 Kyu</option>
     <option>30 Kyu</option>
   </select>
+</section>
+
+<section class="form-element">
+  <label for="age"><?php echo t('age') ?></label>
+  <!-- value nicht sauber, wenn es neu reinkommt -->
+  <select id="age" name="age" value="<?= $data['age'] ?? null ?>">
+    <option selected="" value="0"><?php echo t('pleaseChoose') ?></option>
+    <option value="adult"><?php echo t('adult') ?></option>
+    <option value="student"><?php echo t('student') ?></option>
+    <option value="minor"><?php echo t('minor') ?></option>
+  </select>
+</section>
+
+<section class="form-element">
+  <label for="club"><?php echo t('cityClub') ?></label>
+  <input id="club" name="club" value="<?= $data['club'] ?? null ?>">
+</section>
+
+<h3>Workshop</h3>
+<section class="form-element">
+  <fieldset>
+    <legend><?php echo t('workshopParticipate') ?>.</legend>
+
+    <div class="radio-buttons" style="margin-top: 0.5rem;">
+      <span>
+        <input type="radio" id="yes" name="workshop" value="yes" <?= (isset($data['workshop']) && $data['workshop'] == "yes") ? "checked" : null ?> />
+        <label for="yes"><?php echo t('yes') ?></label>
+      </span>
+
+      <span>
+        <input type="radio" id="no" name="workshop" value="no" <?= (isset($data['workshop']) && $data['workshop'] == "no") ? "checked" : null ?> />
+        <label for="no"><?php echo t('no') ?></label>
+      </span>
+
+      <span>
+        <input type="radio" id="maybe" name="workshop" value="maybe" <?= (isset($data['workshop']) && $data['workshop'] == "maybe") ? "checked" : null ?> />
+        <label for="maybe"><?php echo t('maybe') ?></label>
+      </span>
+    </div>
+  </fieldset>
+</section>
+
+<section class="form-element">
+  <label for="remarks"><?php echo t('remarks') ?></label>
+  <textarea rows="5" cols="43" id="remarks" name="remarks"><?= $data['remarks'] ?? null ?></textarea>
+
 </section>
 
 <div class="honey">
